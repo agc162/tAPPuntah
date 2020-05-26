1f30a50ff092097317586ff0910c49645d191de2
Melanie Mariam Cruz Morgado <32161106+m3cruz@users.noreply.github.com>
2020-05-27 00:43:08 +0200

    Arreglos de lo anterior

--- tAPPuntah/src/app/Models/Event/event.model.ts
+++ tAPPuntah/src/app/Models/Event/event.model.ts
@@ -3,6 +3,7 @@ import {Comment} from 'src/app/Models/Comment/comment.model';
 export interface Event extends Array<Comment> {
     id: string,
     title: string,
+    user: string,
     datePublic: string,
     dateEvent: string,
     description: string,
